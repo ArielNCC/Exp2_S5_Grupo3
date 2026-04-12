22911f25389858f61dd29ca6ad32e74a3ec87279
Nicolas Ariel <ni.cavieres@duocuc.cl>
2026-04-12 17:53:09 -0400

    Ajustar endpoints a guia profesor

--- src/main/java/com/duoc/LearningPlatform/controller/CursoController.java
+++ src/main/java/com/duoc/LearningPlatform/controller/CursoController.java
@@ -11,7 +11,7 @@ import com.duoc.LearningPlatform.model.Curso;
 import com.duoc.LearningPlatform.service.CursoService;
 
 @RestController
-@RequestMapping("/api/v1/cursos")
+@RequestMapping("/api/v1")
 public class CursoController {
 
     @Autowired
@@ -21,47 +21,47 @@ public class CursoController {
         this.service = service;
     }
 
-    @GetMapping
+    @GetMapping("/cursos")
     public ResponseEntity<ArrayList<Curso>> listarTodos() {
         return ResponseEntity.ok(service.listarTodos());
     }
 
-    @GetMapping("/indice/{indice}")
+    @GetMapping("/cursos/indice/{indice}")
     public ResponseEntity<Curso> listarPorIndice(@PathVariable String indice) {
         return service.obtenerPorIndice(indice)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/disponibilidad/{activo}")
+    @GetMapping("/cursos/disponibilidad/{activo}")
     public ResponseEntity<ArrayList<Curso>> listarPorDisponibilidad(@PathVariable boolean activo) {
         return ResponseEntity.ok(service.listarPorDisponibilidad(activo));
     }
 
-    @GetMapping("/categoria/{categoria}")
+    @GetMapping("/cursos/categoria/{categoria}")
     public ResponseEntity<ArrayList<Curso>> listarPorCategoria(@PathVariable String categoria) {
         return ResponseEntity.ok(service.listarPorCategoria(categoria));
     }
 
-    @GetMapping("/profesor/{profesor}")
+    @GetMapping("/cursos/profesor/{profesor}")
     public ResponseEntity<ArrayList<Curso>> listarPorProfesor(@PathVariable String profesor) {
         return ResponseEntity.ok(service.listarPorProfesor(profesor));
     }
 
-    @PostMapping
+    @PostMapping("/cursos")
     public ResponseEntity<Curso> crearCurso(@RequestBody Curso curso) {
         Curso nuevoCurso = service.crearCurso(curso);
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevoCurso); // Retorna 201 Created
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/cursos/{id}")
     public ResponseEntity<Curso> actualizarCurso(@PathVariable String id, @RequestBody Curso curso) {
         return service.actualizarCurso(id, curso)
                 .map(ResponseEntity::ok) // Retorna 200 OK
                 .orElseGet(() -> ResponseEntity.notFound().build()); // Retorna 404 si no existe
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/cursos/{id}")
     public ResponseEntity<Void> eliminarCurso(@PathVariable String id) {
         if (service.eliminarCurso(id)) {
             return ResponseEntity.noContent().build(); // Retorna 204 No Content
