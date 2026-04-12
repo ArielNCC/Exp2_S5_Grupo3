b83abffab62c24d80d396d273531ad710219fac4
Nicolas Ariel <ni.cavieres@duocuc.cl>
2026-04-12 18:13:04 -0400

    Modificar Curso model para eliminar boilerplate

--- src/main/java/com/duoc/LearningPlatform/model/Curso.java
+++ src/main/java/com/duoc/LearningPlatform/model/Curso.java
@@ -6,9 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "cursos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Curso {
 
     @Id
@@ -30,62 +36,11 @@ public class Curso {
     @Column(nullable = false)
     private boolean activo;
 
-    public Curso() {
-    }
-
     public Curso(String indice, String nombre, String categoria, String profesor, boolean activo) {
         this.indice = indice;
         this.nombre = nombre;
         this.categoria = categoria;
         this.profesor = profesor;
-        this.activo = activo;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getIndice() {
-        return indice;
-    }
-
-    public void setIndice(String indice) {
-        this.indice = indice;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-
-    public String getProfesor() {
-        return profesor;
-    }
-
-    public void setProfesor(String profesor) {
-        this.profesor = profesor;
-    }
-
-    public boolean isActivo() {
-        return activo;
-    }
-
-    public void setActivo(boolean activo) {
         this.activo = activo;
     }
 }
